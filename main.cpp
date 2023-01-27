0128542b944001964afdf90f35f4c344d28f50d7
denisioja <92025554+denisioja@users.noreply.github.com>
2023-01-27 19:40:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 #include "Sala.h"
 
 int main() {
+    /*
     Sala *sala = new Sala;
     sala->citire(std::cin);
     sala->afisare(std::cout);
     sala->adaugareAngajat();
-    sala->cautaClient("Popa");
+    sala->cautaClient("Popa");*/
     return 0;
 }
